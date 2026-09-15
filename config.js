75a3f4d4dd104032d0d426a82883b34e33e8de37
Kok <85142934+LargeEgg@users.noreply.github.com>
2026-09-15 16:26:14 -0500

    im stupid

--- config.js
+++ config.js
@@ -1,2 +1,4 @@
-window.SUPABASE_URL = "PutYoShitHere";
-window.SUPABASE_ANON_KEY = "PutYoShitHere";
+// Replace values below with your own
+
+window.SUPABASE_URL = "https://zfijatxqtjwlhxejmnyj.supabase.co";
+window.SUPABASE_ANON_KEY = "sb_publishable_HqsuR_xTl3ldVOm6UpNz2A_jYZdFmzi";
